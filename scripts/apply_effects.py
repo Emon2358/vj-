6e7b22825a7bfce695562b0987395da7d627c1ab
kjgd <144606308+Emon2358@users.noreply.github.com>
2025-07-07 15:03:11 +0900

    Update apply_effects.py

--- scripts/apply_effects.py
+++ scripts/apply_effects.py
@@ -6,12 +6,11 @@ input_path = os.environ['VIDEO_IN']
 output_path = os.environ['VIDEO_OUT']
 filename = os.path.splitext(os.path.basename(input_path))[0]
 
-# 1入力で直列に全エフェクトを通す
+# Remove the overlay filter since we don't have a second input stream
 filter_complex = (
     f"[0:v] "
     f"chromakey=0x00FF00:0.3:0.2, "
     f"format=yuva420p, "
-    f"overlay=10:10, "
     f"select='not(mod(n\\,5))', "
     f"setpts='N/FRAME_RATE/TB', "
     f"drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:"
